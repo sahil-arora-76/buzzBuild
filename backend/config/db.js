172c034b2d866fcc7f651c0e6a1cb637f0aa2626
sahil-arora-76 <sahilarora8527397612@gmail.com>
2024-11-23 22:31:58 +0530

    changed mongodb password

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://niharikashekar14:kZskHbXSeLghchp6@cluster0.racb7co.mongodb.net/');
+    await mongoose.connect('mongodb+srv://niharikashekar14:voltTHOR11@cluster0.racb7co.mongodb.net/');
     console.log('db connected   ')
 
 }
